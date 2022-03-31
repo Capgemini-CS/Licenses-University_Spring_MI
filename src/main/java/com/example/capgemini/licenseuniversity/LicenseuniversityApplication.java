a8c8af25cc8873795ffc2d08aa83c1d993979296
IMARGHIO <ioan.marghioala@capgemini.com>
2022-03-31 11:08:20 +0300

    Initial commit

---
+++ src/main/java/com/example/capgemini/licenseuniversity/LicenseuniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.capgemini.licenseuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseuniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capgemini/licenseuniversity/LicenseuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capgemini.licenseuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
